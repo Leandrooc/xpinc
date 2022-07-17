73e498d1d38d2a07c0e0fbcd3a3a78498a159805
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2022-07-17 01:25:13 -0300

    feat: :sparkles: new Client Entity

---
+++ TypeScript/src/entities/Client.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('clients')
+export default class Client {
+  @PrimaryGeneratedColumn()
+    id: number;
+
+  @Column({ type: 'text' })
+    name: string;
+
+  @Column({ type: 'decimal' })
+    balance: number;
+}
