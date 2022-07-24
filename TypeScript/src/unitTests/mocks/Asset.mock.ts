75f46921abf154c07ad89583b740efe7a18f5abc
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2022-07-24 16:14:59 -0300

    test: fix Assets mock 😁 - after change on investment return

--- TypeScript/src/unitTests/mocks/Asset.mock.ts
+++ TypeScript/src/unitTests/mocks/Asset.mock.ts
@@ -14,25 +14,14 @@ const findAndCount = [
   2,
 ];
 
-const getAssetsExpectedResult = [
-  {
-    numberOfAssets: 2,
-  },
-  [
-    {
-      id: 1,
-      name: 'XPX',
-      quantity: 6,
-      value: '1.000000',
-    },
-    {
-      id: 2,
-      name: 'XPI',
-      quantity: 10,
-      value: '3.000000',
-    },
-  ],
-];
+const getAssetsExpectedResult = {
+  numberOfAssets: 2,
+  assets: [{
+    id: 1, name: 'XPX', quantity: 6, value: '1.000000',
+  }, {
+    id: 2, name: 'XPI', quantity: 10, value: '3.000000',
+  }],
+};
 
 const findOne = {
   id: 1,
